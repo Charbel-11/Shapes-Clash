7338b2a835211ce39ed11761afbacb8f1e08d10f
Charbel Chucri <charbel11chucri@gmail.com>
2021-05-23 18:09:15 +0300

    Fixed destroy mini collision:

--- Assets/NewAnims/MiniFall.cs
+++ Assets/NewAnims/MiniFall.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class MiniFall : MonoBehaviour {
     public FallInPieces Fall;
-
-    private void OnTriggerEnter(Collider col)
-    {
-        if(col.tag == "Player")
-        {
-            Fall.Fall();
+    void Start() {
+        if (GetComponent<Shape_Player>()) {
+            GameObject miniEnemy = transform.Find("MiniEnemy").gameObject;
+            Shape_Player otherPlayer = GameObject.Find(name == "Player1" ? "Player2" : "Player1").GetComponent<Shape_Player>();
+            if (otherPlayer is Cube_Player)
+                Fall = miniEnemy.transform.Find("MiniCube").GetComponent<FallInPieces>();
+            else if (otherPlayer is Pyramid_Player)
+                Fall = miniEnemy.transform.Find("MiniPyramid").GetComponent<FallInPieces>();
+            else if (otherPlayer is Star_Player)
+                Fall = miniEnemy.transform.Find("MiniStar").GetComponent<FallInPieces>();
+            else if (otherPlayer is Sphere_Player)
+                Fall = miniEnemy.transform.Find("MiniSphere").GetComponent<FallInPieces>();
         }
+    }
+    public void fall() { 
+        Fall.Fall();
     }
 }
